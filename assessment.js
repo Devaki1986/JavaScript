51c93b6c0e010154e3dc1420793491cf7d2239aa
Devaki1986 <50104493+Devaki1986@users.noreply.github.com>
2023-12-20 18:45:36 +0530

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -1,5 +1,7 @@
+
 const process = require('process');
 let data = process.cwd()+"/heartrate.json"
+let result_set=process.cwd()+"/Result.json"
 let fs = require(data);
 var length = fs.length -1 ;
 var max =0;
@@ -7,6 +9,7 @@ var min =0
 var median =0
 var i=0
 var j=0
+console.log("cuurent wd" +process.cwd());
 console.log(fs[i+1].timestamps.startTime.substring(0,10));
 var result ={}
 var result_array=[]
@@ -46,7 +49,23 @@ while(  i < length)
                     )
                     i++;
                     j=0;
-             }          
-}   
+
+
+             }
+
+             
+}
+    
 result.result_array = result_array;
 console.log(result_array);
+fs = require('fs')
+let json = JSON.stringify(result_array);
+fs.writeFile('Result.json',json,'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+    console.log("JSON file has been saved.");
+});
+
+
